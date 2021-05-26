1fbb11c5a26ce05c0b5af59f7b18c488fd328345
JoseZaq <jose.zaquinaula.7e4@itb.cat>
2021-05-26 21:06:31 +0200

    fox: can die

--- src/Animal.java
+++ src/Animal.java
@@ -1,18 +1,25 @@
 abstract class Animal{
     // vars
     int x, y;
+    boolean mort;
     // contructors
     public Animal(int x, int y){
         this.x = x;
         this.y = y;
+        mort = false;
     }
     // methods
+    public void mor() {
+        this.mort = true;
+    }
     public abstract String toString();
     public void mou(){
+        if(mort) return;
         Tile[][] tauler = EatRabbitSim.tauler;
         int i = (int) (Math.random() * 3) - 1 +x;
         int j = (int) (Math.random() * 3) - 1 +y;
-        if(i >= 0 && i < tauler.length && j >= 0 && j < tauler[0].length && !tauler[i][j].esAigua() && !tauler[i][j].esAnimal()){
+        if(i >= 0 && i < tauler.length && j >= 0 && j < tauler[0].length &&
+                !tauler[i][j].esAigua() && !tauler[i][j].esAnimal()){
             tauler[x][y].delAnimal();
             tauler[i][j].setAnimal(this);
             x=i; y=j;
--- src/Fox.java
+++ src/Fox.java
@@ -1,8 +1,32 @@
 class Fox extends Animal{
     //vars
+    int energia = 10;
     // contructor
     public Fox(int x, int y){ super(x, y); } // reutilitzem el constructor de la classe mare
     // methods
+    public void mou(){
+        Tile[][] tauler = EatRabbitSim.tauler;
+        if(energia == 0){
+            mor();                      // eliminem les referències a aquest objecte i confiem en el
+            tauler[x][y].delAnimal();   // garbage collector per a destruir-lo (ja que no té referències)
+        }else {
+            int i = (int) (Math.random() * 3) - 1 + x;
+            int j = (int) (Math.random() * 3) - 1 + y;
+            boolean dinsTauler = i >= 0 && i < tauler.length && j >= 0 && j < tauler[0].length;
+            if (dinsTauler && !tauler[i][j].esAigua() && !tauler[i][j].esFox() &&
+                    !(tauler[i][j].esRabbit() && tauler[i][j].esRock())) {
+                if (tauler[i][j].esRabbit()) {
+                    tauler[i][j].getAnimal().mor();
+                    energia += 10;
+                }
+                tauler[x][y].delAnimal();
+                tauler[i][j].setAnimal(this);
+                x = i;
+                y = j;
+                energia --;
+            }
+        }
+    }
     @Override
     public String toString() { return "\uD83D\uDC3A"; } // icona de llop
 }
